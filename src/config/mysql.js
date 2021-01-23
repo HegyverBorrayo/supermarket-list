83f97ab01f64f9523885acd96811c95452259e31
Erinson Borrayo <erinson.borrayo@galileo.edu>
2021-01-23 10:26:40 -0600

    Configuracion para deploy en prod

--- src/config/mysql.js
+++ src/config/mysql.js
@@ -1,11 +1,18 @@
 const mysql = require("mysql");
 
-const conn = mysql.createConnection({
+/*const conn = mysql.createConnection({
     host: "localhost",
     user: 'erehebo',
     password: "2013043-eB",
     database: 'lista_super'
-});
+});*/
+
+const conn = mysql.createConnection({
+    host: "z5zm8hebixwywy9d.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: 's1995pfkk7a5csmj',
+    password: "ucycm9zsz56cyzd1",
+    database: 'hvu07lntfsc1b12j'
+})
 
 conn.connect(err => {
     if(err) console.log("problema en conexion a mysql");
